02da1a22741106cdd22b79d10ac4aceb6e4d030e
Aaron Maxwell <amax@redsymbol.net>
2015-02-21 06:13:39 -0800

    Add a task

--- test_todoserver.py
+++ test_todoserver.py
@@ -2,38 +2,52 @@ import unittest
 import json
 import todoserver
 
+todoserver.app.testing = True
+
 def load_json(data):
     return json.loads(data.decode('utf-8'))
 
-class FlaskTestCase(unittest.TestCase):
+class TestTodo(unittest.TestCase):
     def setUp(self):
         self.app = todoserver.app.test_client()
         todoserver.store.clear()
 
-class TestTodo(FlaskTestCase):
-    def test_get_tasks(self):
+    def test_get_empty_task_list(self):
         resp = self.app.get('/items/')
         self.assertEqual(200, resp.status_code)
         self.assertEqual([], load_json(resp.data))
-        
-    def test_describe_task(self):
-        resp = self.app.get('/items/42/')
+
+    def test_add_one_task(self):
+        task_summary = 'Buy milk'
+        task_description = 'Lots and lots of delicious milk!'
+        # check test assumption
+        resp = self.app.get('/items/')
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(b'GET /items/42/', resp.data)
-
-    def test_add_task(self):
-        resp = self.app.post('/items/')
+        self.assertEqual([], load_json(resp.data))
+        # add an item
+        resp = self.app.post(
+            '/items/',
+            content_type='application/json',
+            data = json.dumps({
+                'summary': task_summary,
+                'description': task_description,
+        }))
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(b'POST /items/', resp.data)
-
-    def test_task_done(self):
-        resp = self.app.delete('/items/42/')
+        returned = load_json(resp.data)
+        self.assertEqual(dict, type(returned))
+        self.assertIn('id', returned)
+        task_id = returned['id']
+        # fetch this one task
+        resp = self.app.get('/items/{:d}/'.format(task_id))
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(b'DELETE /items/42/', resp.data)
-
-    def test_update_task(self):
-        resp = self.app.put('/items/42/')
+        returned = load_json(resp.data)
+        self.assertEqual(dict, type(returned))
+        self.assertEqual(task_id, returned['id'])
+        self.assertEqual(task_summary, returned['summary'])
+        self.assertEqual(task_description, returned['description'])
+        # fetch all tasks
+        resp = self.app.get('/items/')
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(b'PUT /items/42/', resp.data)
-
-
+        returned = load_json(resp.data)
+        self.assertEqual(1, len(returned))
+        self.assertEqual(task_id, returned[0]['id'])
--- todoserver.py
+++ todoserver.py
@@ -1,43 +1,53 @@
 import json
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+    )
 app = Flask(__name__)
-
 class TaskStore:
     def __init__(self):
-        self.last_id = 0
-        self.tasks = {}
+        self.clear()
+
+    def new_id(self):
+        id = self._last_id
+        self._last_id += 1
+        return id
 
     def add(self, summary, description):
-        task_id = self.last_id
-        self.last_id += 1
+        task_id = self.new_id()
         task = {
             'id': task_id,
             'summary': summary,
             'description': description,
             }
         self.tasks[task_id] = task
-        return task
+        return task_id
 
     def all_tasks(self):
         return list(self.tasks.values())
 
     def clear(self):
-        self.last_id = 0
+        self._last_id = 0
         self.tasks = {}
         
 store = TaskStore()
 
 @app.route('/items/', methods=['GET'])
 def get_tasks():
-    return json.dumps(store.all_tasks())
+    return json.dumps([
+    {'id': task['id'], 'summary': task['summary']}
+        for task in store.all_tasks()])
 
-@app.route('/items/<task_id>/')
-def describe_task(task_id, methods=['GET']):
-    return 'GET /items/{}/'.format(task_id)
+@app.route('/items/<task_id>/', methods=['GET'])
+def describe_task(task_id):
+    task_id = int(task_id)
+    return json.dumps(store.tasks[task_id])
 
 @app.route('/items/', methods=['POST'])
 def add_task():
-    return 'POST /items/'
+    data = request.get_json()
+    task_id = store.add(data['summary'], data['description'])
+    return json.dumps({'id': task_id})
 
 @app.route('/items/<task_id>/', methods=['DELETE'])
 def task_done(task_id):
@@ -49,4 +59,4 @@ def update_task(task_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
